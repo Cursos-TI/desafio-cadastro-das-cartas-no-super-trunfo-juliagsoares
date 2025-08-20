e41ecd0ef3c226ef640e730575f2b25b037bf33b
juliagsoares <julia_gouvea@outlook.com>
2025-08-20 00:01:49 +0000

    Código Super Trunfo da Júlia

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,100 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Propriedades da primeira carta.
+char estado_carta1;
+char codigo_carta_carta1[5];
+char nome_cidade_carta1[20];
+int populacao_carta1;
+float area_carta1;
+float pib_carta1;
+int pontos_turisticos_carta1;
+
+// Propriedades da segunda carta.
+char estado_carta2;
+char codigo_carta_carta2[5];
+char nome_cidade_carta2[20];
+int populacao_carta2;
+float area_carta2;
+float pib_carta2;
+int pontos_turisticos_carta2;
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Lendo propriedades da primeira carta.
+    printf("Carta 1: \n");
+    printf("Digite o Estado: \n");
+    scanf("%s", &estado_carta1);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", &codigo_carta_carta1);
+
+    printf("Digite o nome da Cidade: \n");
+    // Usei isso pois estava dando problema com o espaço no meio do nome.
+    scanf(" %[^\n]", &nome_cidade_carta1);
+
+    printf("Digite a população: \n");
+    scanf("%d", &populacao_carta1);
+
+    printf("Digite a área: \n");
+    scanf("%f", &area_carta1);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib_carta1);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &pontos_turisticos_carta1);
+
+     // Lendo propriedades da segunda carta.
+    printf("Carta 2: \n");
+    printf("Digite o Estado: \n");
+    scanf("%s", &estado_carta2);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", &codigo_carta_carta2);
+
+    printf("Digite o nome da Cidade: \n");
+    // Usei isso pois estava dando problema com o espaço no meio do nome.
+    scanf(" %[^\n]", &nome_cidade_carta2);
+
+    printf("Digite a população: \n");
+    scanf("%d", &populacao_carta2);
+
+    printf("Digite a área: \n");
+    scanf("%f", &area_carta2);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib_carta2);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &pontos_turisticos_carta2);
+
+    // Imprimindo linha em branco.
+    printf("\n");
+
+    // Imprimindo informações da primeira carta.
+    printf("Carta 1: \n");
+    printf("Estado: %c \n", estado_carta1);
+    printf("Código: %s \n", codigo_carta_carta1);
+    printf("Nome da Cidade: %s \n", nome_cidade_carta1);
+    printf("População: %d \n", populacao_carta1);
+    printf("Área: %f km² \n", area_carta1);
+    printf("PIB: %f bilhões de reais \n", pib_carta1);
+    printf("Número de pontos turísticos: %d \n", pontos_turisticos_carta1);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Imprimindo linha em branco.
+    printf("\n");
+
+    // Imprimindo informações da segunda carta.
+    printf("Carta 2: \n");
+    printf("Estado: %c \n", estado_carta2);
+    printf("Código: %s \n", codigo_carta_carta2);
+    printf("Nome da Cidade: %s \n", nome_cidade_carta2);
+    printf("População: %d \n", populacao_carta2);
+    printf("Área: %f km² \n", area_carta2);
+    printf("PIB: %f bilhões de reais \n", pib_carta2);
+    printf("Número de pontos turísticos: %d \n", pontos_turisticos_carta2);
+
+    // Imprimindo linha em branco.
+    printf("\n");
 
     return 0;
 }
